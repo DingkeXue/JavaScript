24622e2bfbf37d6638604b2183b2d2b826bc5620
Xue <605534423@qq.com>
2019-05-22 19:58:51 +0800

    WebSocket

---
+++ WebSockets/main.js
@@ -0,0 +1,35 @@
+/*
+* 同源策略对 web sockets 不适用
+* readyState 有四个值
+* WebSocket.OPENING(0): 正在建立连接
+* WebSocket.OPEN(1): 已建立连接
+* WebSocket.CLOSING(2): 正在关闭连接
+* WebSocket.CLOSE(3): 已经关闭连接
+* WebSocket 只能发送纯文本数据，所以发送前必须进行序列化
+* JavaScript高级程序设计中说WebSocket对象不支持 DOM2级事件，只支持 DOM0 级事件
+* */
+function WebSocketTest() {
+    if ('WebSocket' in window) {
+        alert("正在创建WebSocket");
+
+        // 创建一个 WebSocket 对象
+        var ws = new WebSocket('ws://localhost:8080'); // 不再是HTTP://   ，传入的路径必须是绝对路径
+
+        ws.onopen = function () {
+            // 已建立连接
+            ws.send(JSON.stringify({name: "lbb"})); // 只能发送文本数据，需要序列化
+            alert("正在发送数据");
+        };
+
+        ws.onmessage = function (event) {
+            var msg = event.data; // 传回来的数据在 event 的 data 中
+            alert("已接收数据");
+        };
+
+        ws.onclose = function () {
+            alert("连接已关闭");
+        }
+    } else {
+        alert("创建 WebSocket 失败");
+    }
+}
