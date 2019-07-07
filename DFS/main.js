a56e0e4e262be8f08ca1f452f57da6d7bc09f9e2
DingkeXue <605534423@qq.com>
2019-07-07 21:56:09 +0800

    深度优先搜索

--- DFS/main.js
+++ DFS/main.js
@@ -2,3 +2,18 @@
 * 深度优先搜索
 * 先判断是否有孩子，如果有孩子继续搜索孩子
 * */
+(function () {
+    let root = document.querySelector('.root');
+
+    function DFS(root, list) {
+        if (root) {
+            list.push(root);
+            var children = root.children;
+            for (var i = 0; i < children.length; i++) {
+                DFS(children[i], list);
+            }
+        }
+        return list;
+    }
+    console.log(DFS(root, []));
+})();
