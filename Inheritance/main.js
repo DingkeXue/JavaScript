ab32883f397abe48ab24ce8ae9a450830c4d487e
DingkeXue <605534423@qq.com>
2019-07-09 16:49:26 +0800

    继承机制

--- Inheritance/main.js
+++ Inheritance/main.js
@@ -15,3 +15,30 @@ Children1.prototype = new Parent1();
 let a1 = new Parent1();
 let b1 = new Children1();
 b1.say('hello'); // hello
+
+
+// 组合继承：1.在子类构造函数中继承父类属性 2.将子类 prototype 属性设置为父类实例
+function Parent2(name) {
+    this.name = name;
+}
+Parent2.prototype.say = function () {
+    console.log(this.name);
+};
+
+function Children2(name, age) {
+    // 构造函数内继承
+    Parent2.call(this, name);
+    this.age = age;
+}
+// 原型继承
+Children2.prototype = new Parent2();
+Children2.prototype.sayAge = function () {
+    console.log(this.age);
+};
+
+// 实例
+let a2 = new Parent2('bar');
+let b2 = new Children2('foo', 18);
+a2.say();
+b2.say();
+b2.sayAge();
