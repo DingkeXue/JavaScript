8119c1f8472253c74e1bc98109e90dd4ecea56ef
DingkeXue <605534423@qq.com>
2019-07-09 22:22:46 +0800

    防抖

--- Debounce/debounce.js
+++ Debounce/debounce.js
@@ -4,20 +4,21 @@
 
 ;(function () {
     let button = document.querySelector("#click");
-    button.addEventListener('click', Debounce(sayDebounce));
+    button.addEventListener('click', Debounce(sayDebounce, 1000));
 
     function sayDebounce() {
         alert("点我了！");
     }
 
-    function Debounce(callback) {
+    function Debounce(callback, delay) {
         let timeId = null;
         return function () {
             // 每次触发该函数时将之前的计时清零后重新计时
+            let that = this, args = arguments;
             clearTimeout(timeId);
             timeId = setTimeout(() => {
-                callback.call(this. arguments);
-            }, 1000);
+                callback.call(that, args);
+            }, delay);
         }
     }
 })();
