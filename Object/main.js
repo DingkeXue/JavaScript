245e7a5d804500ee713d3e3a80ef3154407b217a
DingkeXue <605534423@qq.com>
2019-07-21 21:42:16 +0800

    ES6

--- Object/main.js
+++ Object/main.js
@@ -4,3 +4,11 @@
 // Object.is(): 比较两个值是否相等，采用 同值相同 算法来解决 ES5中 NaN 不等于自身，以及 +0 等于 -0 的情况
 Object.is('foo', 'foo');  // true
 Object.is({}, {});  // false 引用类型，指针不同
+
+// ES5
+console.log(+0 === -0);  // true
+console.log(NaN === NaN);  // false
+
+// ES6
+Object.is(+0, -0); // false
+Object.is(NaN, NaN); // true
