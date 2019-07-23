87a702df38754a16e72dec44756376fb906b9b17
DingkeXue <605534423@qq.com>
2019-07-23 22:01:14 +0800

    promise封装ajax

---
+++ Promise/main.js
@@ -0,0 +1,19 @@
+/*
+*  使用 promise 封装 ajax
+* */
+function ajax(url) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+                    resolve(xhr.responseText);
+                } else {
+                    reject();
+                }
+            }
+        };
+        xhr.open("GET", url, true);
+        xhr.send(null);
+    })
+}
