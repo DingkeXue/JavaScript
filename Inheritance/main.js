a4d01872c9d7e9643cf7a030ce298fe1bb376789
DingkeXue <605534423@qq.com>
2019-07-24 16:40:59 +0800

    创建对象的方式

--- Inheritance/main.js
+++ Inheritance/main.js
@@ -1,8 +1,30 @@
 /*
-* 这里说了三种继承机制，分别是 原型链继承、混合继承、寄生组合继承
+* 这里说了五种继承机制，分别是 原型链继承、混合继承、寄生组合继承
 * */
+// 工厂模式: 对象无法识别，因为所有的实例都指向一个原型
+function Parent0(name) {
+    let o = new Object();
+    o.name = name;
+    o.getName = function () {
+        console.log(this.name);
+    };
+    return o;
+}
+let person = Parent0('lbb');
+console.log(person);
 
-// 原型链继承：将子类的 prototype 属性设置为 A 的实例，要确保构造函数没有任何参数；弊端是不支持多重继承
+
+// 构造函数模式缺点：每次创建实例时，每个方法都要被创建一次
+function Parent00(name) {
+    this.name = name;
+    this.getName = function () {
+        console.log(this.name);
+    }
+}
+let person2 = new Parent00('lbb');
+console.log(person2.getName());
+
+// 原型继承缺点：所有的属性和方法都共享，不能初始化参数
 function Parent1() {}
 Parent1.prototype.say = function (msg) {
   console.log(msg);
@@ -35,7 +57,6 @@ Children2.prototype = new Parent2();
 Children2.prototype.sayAge = function () {
     console.log(this.age);
 };
-
 // 实例
 let a2 = new Parent2('bar');
 let b2 = new Children2('foo', 18);
@@ -47,7 +68,7 @@ b2.sayAge();
 function inheritPrototype(sub, sup) {
     let prototype = Object(sup.prototype);
     // 重写因子类原型导致子类 constructor 属性被修改
-    sub.constructor = sub;
+    prototype.constructor = sub;
     // 设置子类的原型
     sub.prototype = prototype;
 }
