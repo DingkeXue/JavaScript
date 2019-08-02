6a1df978fe1e4c75c6252696e2856d038c642255
DingkeXue <605534423@qq.com>
2019-08-02 19:49:44 +0800

    深复制

--- DeepClone/main.js
+++ DeepClone/main.js
@@ -2,15 +2,15 @@
 * 深复制：判断属性是否属于 object 后者 array，如果属于，递归复制；不属于，直接复制
 * */
 function deepClone(origin, target={}) {
+    // 遍历origin中的key
     for(let key in origin) {
+        // 看改属性是否是对象上而不是原型上的
         if (origin.hasOwnProperty(key)) {
-            // 如果属性是对象并且不为空
+            // 该属性是对象的情景
             if (typeof origin[key] === 'object' && origin[key] !== null) {
                 target[key] = {};
                 deepClone(origin[key], target[key]);
-            }
-            // 如果属性是数组
-            else if (Array.isArray(key)) {
+            } else if (Array.isArray(origin[key])) {  // 是数组的情况
                 target[key] = [];
                 deepClone(origin[key], target[key]);
             } else {
@@ -34,3 +34,7 @@ let copy = {};
 console.log(deepClone(obj, copy));
 obj.name = 'foo';
 console.log(copy);  // copy.name: bar 不变
+
+// 方法二
+let copy2 = JSON.parse(JSON.stringify(obj));
+console.log(copy2);
