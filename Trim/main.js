16e5d6f7155c4377a12bd8c8bc50821a2065e41d
DingkeXue <605534423@qq.com>
2019-08-14 11:57:24 +0800

    去除字符串中的空格

---
+++ Trim/main.js
@@ -0,0 +1,20 @@
+/*
+* 去掉字符串中的字符，要去分别能去掉前、后、前后、中间的空格
+* */
+function trim(str, position) {
+    switch (position) {
+        case 'left':
+            return str.replace(/^\s*/, '');
+        case 'right':
+            return str.replace(/(\s*$)/g, '');
+        case 'both':
+            return str.replace(/^\s*|\s*$/g, '');
+        case 'middle':
+            return str.match(/(^\s*)/g)[0] + str.match(/(\s*$)/g)[0] + str.replace(/\s*/g, '');
+        default:
+            return str.replace(/\s*/g, '');
+    }
+}
+
+console.log('trim:', trim('       sd  d', 'middle'));
+
