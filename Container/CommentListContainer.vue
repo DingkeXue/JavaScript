f0a9187e27a91b9e4ff9ded22a3fd7e3d9380ef2
DingkeXue <605534423@qq.com>
2019-08-29 17:26:11 +0800

    '容器组件和展示组件'

---
+++ Container/CommentListContainer.vue
@@ -0,0 +1,28 @@
+<!--容器组件。负责获取store中参数来传递给展示组件，通过methods定义回到函数，回调函数内部调用dispatch方法-->
+<template>
+    <CommentList
+        :comments="comments"
+        :fetch="fetchComments"
+    ></CommentList>
+</template>
+
+<script>
+    import CommentList from './CommentListNew'
+    export default {
+        name:'CommentListContainer',
+        components: {
+            CommentList
+        },
+        computed: {
+            comments() {
+                return this.$store.state.comments;
+            }
+        },
+
+        methods: {
+            fetchComments() {
+                return this.$store.dispatch('fetchComments')
+            }
+        }
+    }
+</script>
---
+++ Container/CommentListNew.vue
@@ -0,0 +1,33 @@
+<!--使用容器组件。该组件为展示评论列表，但是，只显示容器组件通过props传过来的值，不涉及到数据的操作-->
+<template>
+    <ul>
+        <li v-for="comment in comments" :key="comment.id">
+            {{comment.body}}
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'CommentListNew',
+
+        // 接收容器组件传过来的参数
+        props: {
+            comments: {
+                type: Array,
+                default () {
+                    return []
+                }
+            },
+            fetch: {
+                type: Function,
+                default: () => {}
+            }
+        },
+
+        // 定义回调函数来触发容器组件内的获取store中方法
+        mounted() {
+            this.fetch();
+        }
+    }
+</script>
