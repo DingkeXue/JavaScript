e56acaaee33660595ff64597c4660c4b1fa016ac
DingkeXue <605534423@qq.com>
2019-08-29 17:06:38 +0800

    '容器组件和展示组件'

---
+++ Container/CommentList.vue
@@ -0,0 +1,22 @@
+<!--不使用容器组件来展示评论,该组件和vuex产生了耦合，不利于复用-->
+<template>
+    <ul>
+        <li v-for="comment in comments" :key="comment.id">
+            {{comment.body}}
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'CommentList',
+        computed: {
+            comments() {
+                return this.$store.state.comments;
+            }
+        },
+        mounted() {
+            this.$store.dispatch('fetchComments')
+        }
+    }
+</script>
